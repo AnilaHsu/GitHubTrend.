700239d9f45ab5d5343c8afdc7149b1f172b7261
simple1017 <simple01281017@gmail.com>
2022-08-15 23:07:48 +0800

    split UserMenu in login components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,184 +1,48 @@
-import { useState } from "react";
-import "../style/login.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  solid, regular, brands,
-} from "@fortawesome/fontawesome-svg-core/import.macro";
-
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-
-import { useDispatch,useSelector } from "react-redux";
-import { openDialog } from "../features/dialog"
-import { logout,selectMenu } from "../features/user"
-
+import { regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { useDispatch, useSelector } from "react-redux";
+import { openDialog } from "../features/dialog";
+import { UserMenu } from "./UserMenu";
 
 export function Login() {
   const dispatch = useDispatch();
-  const loginInfo = useSelector((state) => state.user.value.loginInfo)
-
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const loginInfo = useSelector((state) => state.user.value.loginInfo);
 
   if (loginInfo.login === true) {
     if (loginInfo.photo === null) {
       return (
-        <div className="login">
-        <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-           <FontAwesomeIcon
-            icon={regular("circle-user")}
-            className="user-icon"
-          />
-          </IconButton>
-        </Tooltip>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open} onClose={handleClose} onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-              mt: 1.5,
-              "& .MuiAvatar-root": {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-              "&:before": {
-                content: '""',
-                display: "block",
-                position: "absolute",
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: "background.paper",
-                transform: "translateY(-50%) rotate(45deg)",
-                zIndex: 0,
-              },
-            },
-          }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-        >
-          <MenuItem onClick={() => {
-            dispatch(selectMenu("setting"))
-          }}>
-            <ListItemIcon>
-              <Settings fontSize="small" />
-            </ListItemIcon>
-            Settings
-          </MenuItem>
-          <MenuItem onClick={() => {
-            dispatch(logout({login:false}))
-            dispatch(selectMenu("logout"))
-          }}>
-            <ListItemIcon>
-              <Logout fontSize="small" />
-            </ListItemIcon>
-            Logout
-          </MenuItem>
-        </Menu>
-      </div>
+        <UserMenu
+          avatar={
+            <FontAwesomeIcon
+              icon={regular("circle-user")}
+              className="user-icon"
+            />
+          }
+        />
       );
     } else {
       return (
-        <div className="login">
-          <Tooltip title="Account settings">
-            <IconButton
-              onClick={handleClick}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open ? "account-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-            >
-              <img src={loginInfo.photo} className="user-icon" />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            anchorEl={anchorEl}
-            id="account-menu"
-            open={open} onClose={handleClose} onClick={handleClose}
-            PaperProps={{
-              elevation: 0,
-              sx: {
-                overflow: "visible",
-                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                mt: 1.5,
-                "& .MuiAvatar-root": {
-                  width: 32,
-                  height: 32,
-                  ml: -0.5,
-                  mr: 1,
-                },
-                "&:before": {
-                  content: '""',
-                  display: "block",
-                  position: "absolute",
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: "background.paper",
-                  transform: "translateY(-50%) rotate(45deg)",
-                  zIndex: 0,
-                },
-              },
-            }}
-            transformOrigin={{ horizontal: "right", vertical: "top" }}
-            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          >
-            <MenuItem onClick={() => {
-              dispatch(selectMenu("setting"))
-            }}>
-              <ListItemIcon>
-                <Settings fontSize="small" />
-              </ListItemIcon>
-              Settings
-            </MenuItem>
-            <MenuItem onClick={() => {
-              dispatch(logout({login:false}))
-              dispatch(selectMenu("logout"))
-            }}>
-              <ListItemIcon>
-                <Logout fontSize="small" />
-              </ListItemIcon>
-              Logout
-            </MenuItem>
-          </Menu>
-        </div>
+        <UserMenu
+          avatar={
+            <img alt="avatar" src={loginInfo.photo} className="user-icon" />
+          }
+        />
       );
     }
-  } else if (loginInfo.login === false ) {
+  } else if (loginInfo.login === false) {
     return (
       <div className="login">
-        <button className="login-button" onClick={() => {dispatch(openDialog(true))}}>
+        <button
+          className="login-button"
+          onClick={() => {
+            dispatch(openDialog(true));
+          }}
+        >
           登入 / 註冊
         </button>
       </div>
     );
   }
 }
+
+export default Login;
