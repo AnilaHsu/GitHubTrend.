4734d285935ffb10c0373a28c05697d3896c61b9
simple1017 <simple01281017@gmail.com>
2022-08-30 20:05:24 +0800

    formatting the code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,46 +1,50 @@
 import { useSelector } from "react-redux";
-import  "../style/homePage.scss"
-import TrendFilter from "../components/TrendFilter"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular } from '@fortawesome/fontawesome-svg-core/import.macro'
-
+import "../style/homePage.scss";
+import TrendFilter from "../components/TrendFilter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export function HomePage() {
   const trendingData = useSelector((state) => state.trend.value);
   const listItems = trendingData.map((trend, index) => {
-    return(
-        <div className="trend-row" key={index}>
-            <div className="trend-left">
-                <h2>{trend.title}</h2>
-                <p>{trend.introduction}</p>    
-            </div>
-            <div className="trend-right">
-                <div className="icon">
-                    <p><FontAwesomeIcon icon={solid("code")} /></p>
-                    <p><FontAwesomeIcon icon={regular("star")} /></p>
-                    <p><FontAwesomeIcon icon={solid("code-fork")} /></p>
-                </div>
-                <div className="text">
-                    <p>{trend.language}</p>
-                    <p>{trend.total_star}</p>
-                    <p>{trend.fork}</p>
-                </div>
-            </div>       
+    return (
+      <div className="trend-row" key={index}>
+        <div className="trend-left">
+          <h2>{trend.title}</h2>
+          <p>{trend.introduction}</p>
         </div>
-    )       
+        <div className="trend-right">
+          <div className="icon">
+            <p>
+              <FontAwesomeIcon icon={solid("code")} />
+            </p>
+            <p>
+              <FontAwesomeIcon icon={regular("star")} />
+            </p>
+            <p>
+              <FontAwesomeIcon icon={solid("code-fork")} />
+            </p>
+          </div>
+          <div className="text">
+            <p>{trend.language}</p>
+            <p>{trend.total_star}</p>
+            <p>{trend.fork}</p>
+          </div>
+        </div>
+      </div>
+    );
   });
-  return <div className="trend-box">
-            <div>
-                <h1 className="trend-title">Trending</h1>
-                
-            </div>
-            <div className="trend-content">
-                <TrendFilter />
-                <div className="trend-table">
-                    {listItems}
-                </div>
-            </div>
-        </div>;
+  return (
+    <div className="trend-box">
+      <div>
+        <h1 className="trend-title">Trending</h1>
+      </div>
+      <div className="trend-content">
+        <TrendFilter />
+        <div className="trend-table">{listItems}</div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
