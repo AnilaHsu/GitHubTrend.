ef1b7c552691abf67aac3b4caeb30f0c0ef92f5e
anilahsu <sss880128gmail.com>
2022-09-23 16:49:23 +0800

    added the MUI typofraphy to unified the MUI components' font

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,26 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux";
+import { createTheme, ThemeProvider } from "@mui/material";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Mali', 'cursive', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      'sans-serif'
+    ].join(','),
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
