6c8bd4cc22d5dacc7a1e9806d98271dd041434ce
anilahsu <sss880128gmail.com>
2022-09-28 16:03:07 +0800

    feature:move loginState of localStrage to useSlice initialState

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,29 @@ import './style/app.scss';
 import { useEffect } from 'react';
 import { Header } from './components/header/Header';
 import { Dialog } from './components/Dialog';
-import { login } from "./slices/userSlice";
-import { LOGIN_STATE } from './constants/local-storage';
 import { HomePage } from './pages/HomePage'
-import { fetchLanguageData, fetchTrendData } from './slices/trendSlice';
+import { fetchLanguageData, fetchTrendData} from './slices/trendSlice';
 import { useAppDispatch, useAppSelector } from "./redux";
-import { LoginStateType } from './type';
+import Footer from './components/Footer';
 
 
 function App() {
   const dispatch = useAppDispatch();
   const dialog = useAppSelector((state) => state.dialog.open);
-  const lang = useAppSelector((state) => state.trend.langCode);
-  const range  = useAppSelector((state) => state.trend.rangeCode);
+  const langCode = useAppSelector((state) => state.trend.langCode); 
+  const rangeCode = useAppSelector((state) => state.trend.rangeCode);
 
   useEffect(() => {
-    const loginInfoString: string | null = localStorage.getItem(LOGIN_STATE)
-    if (loginInfoString) {
-      const loginInfo: LoginStateType = JSON.parse(loginInfoString);
-      dispatch(login(loginInfo));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchTrendData({lang, range}));
+    dispatch(fetchTrendData({langCode, rangeCode}));
     dispatch(fetchLanguageData());
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <div className="container">
       { dialog && <Dialog /> }
         <Header />
-        
         <HomePage />
+        <Footer />
     </div>
   );
 }
