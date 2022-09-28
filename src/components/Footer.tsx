894db2e930481a4fa7c101f4ef8979e553b183b3
anilahsu <sss880128gmail.com>
2022-09-28 16:07:26 +0800

    feature: add new component Footer and Footer style

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../style/footer.scss"
+
+function Footer() {
+    return (
+        <footer className='footer-container'>
+            <span>Copyright © RealD All rights reserved.</span>
+        </footer>
+    );
+}
+
+export default Footer;
