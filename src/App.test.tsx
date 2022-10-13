e72d048e286435725fd769990363cde531351003
anilahsu <sss880128gmail.com>
2022-10-13 17:38:55 +0800

    feature: update the testing and reportWebVitals

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,6 @@
-import { render, screen } from "@testing-library/react";
-// @ts-expect-error TS(2307): Cannot find module './components/App' or its corre... Remove this comment to see the full error message
-import App from "./components/App";
+import { render } from "@testing-library/react";
+import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
